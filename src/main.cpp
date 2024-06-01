e43fc3d56dd23620d13a64ca2b858f57f171ea25
Imad Laggoune <imad.laggoune.m@gmail.com>
2024-06-01 19:30:12 +0100

    [CHANGE] From pre-alpha to alpha
    It is now functional, It only needs few miner improvements before going beta then release.

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@ MIT License | Copyright (c) 2024 Imad Laggoune
 #include <string>
 
 
-sqlite3* db;
 //char* sqlite_err = nullptr;
 //std::string result;
 
+/*
 int request_sqlite_callback(void* args, int size, char** content, char** name){
 
 std::cout << size << " Size\n";
@@ -32,14 +32,40 @@ result->append("{");
     result ->append( "\n}");
 
     return 0;
+}*/
 
+
+int index = 0; // TODO: make it function varaible, probably std::pair?
+
+int request_sqlite_callback(void* args, int size, char** content, char** name){
+
+std::string* result = (std::string*)args;
+    result->append("\n\t\"");
+    result->append(std::to_string(index));
+    ++index;
+    result->append("\" : {");
+
+    for (int i = 0; i < size; ++i){
+        result->append("\n\t\t\"");
+        result->append(name[i]);
+        result->append( "\" : \"");
+        result->append( content[i]);
+        result->append("\",");
+    }
+    result->pop_back();
+    result ->append( "\n\t\t},"); 
+
+    return 0;
 }
 
 
 
 
 const char* url_decoder(std::string& url_encoded_string){
-
+    
+    
+     ////////// This method is VERY slow compare to the one used now ///////////
+    /*std::chrono::time_point t= std::chrono::high_resolution_clock::now();
     size_t p_position = url_encoded_string.find('%');
     if (p_position > url_encoded_string.size()){
         return url_encoded_string.c_str();
@@ -59,9 +85,13 @@ const char* url_decoder(std::string& url_encoded_string){
                 p_position = url_encoded_string.find('%');
             }
     }
-
+        std::chrono::time_point e= std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration((e-t)) << " Time";
     return url_encoded_string.c_str();
-    // TODO: Compare both in terms of performance.
+*/
+//////////////////////////////////////////////////////////////////////
+
+// Retest this:
     int size = url_encoded_string.length();
 
     for (int i = 0; i < size; ++i){
@@ -69,17 +99,15 @@ const char* url_decoder(std::string& url_encoded_string){
         if (url_encoded_string[i] == '%'){
             if (url_encoded_string[i+1] == '2'){
 
-                if (url_encoded_string[i+1] == '0'){
+                if (url_encoded_string[i+2] == '0'){
                     url_encoded_string[i] = ' ';
                 }
 
-                if (url_encoded_string[i+1] == '2'){
+                if (url_encoded_string[i+2] == '2'){
                     url_encoded_string[i] = '"';
                 }
-                url_encoded_string.erase(url_encoded_string.begin()+i+1,url_encoded_string.begin()+i+2);
+                url_encoded_string.erase(url_encoded_string.begin()+i+1,url_encoded_string.begin()+i+3);
                 size-= 2;
-
-
             }
         }
     }
@@ -87,16 +115,21 @@ const char* url_decoder(std::string& url_encoded_string){
     return url_encoded_string.c_str();
 }
 
-int request_sqlite(std::string sql, std::string &result, int &crow_err){
+int request_sqlite(sqlite3* &db, std::string sql, std::string &result, int &crow_err){
+    index = 0;
 
     const char* sql_c = url_decoder(sql);    
     char* sqlite_err = nullptr;
-    
-    std::cout << "Requesting: " << sql_c << "\n";
-    int err = sqlite3_exec(db,sql_c,request_sqlite_callback,&result, &sqlite_err);
-    std::cout << "Done\n";
-        std::cout << result << "\n";
-        
+    int err = 0;
+    if(true){
+        result = "{";
+        err = sqlite3_exec(db,sql_c,request_sqlite_callback,&result, &sqlite_err);
+        if (result != "{")
+             result.pop_back();
+        result += "\n}";
+        }
+    else
+        err = sqlite3_exec(db,sql_c,request_sqlite_callback,&result, &sqlite_err);        
 
     if (err == SQLITE_OK){
         return 0;
@@ -112,11 +145,21 @@ int request_sqlite(std::string sql, std::string &result, int &crow_err){
 
 
 
-int main(){
-    std::cout << "Initalizing CrowSqlite 0.1v-pre-alpha";
+int main(int argc, char** argv){
 
-    if(sqlite3_open_v2("C:/Users/Work/Documents/CrowSqlite/test.db",&db,SQLITE_OPEN_READWRITE,nullptr) != SQLITE_OK){
-        std::cout << sqlite3_open_v2("C:/Users/Work/Documents/CrowSqlite/test.db",&db,SQLITE_OPEN_READWRITE,nullptr);
+    std::string PATH; // NEED absolute path, TODO: Fix this
+    argc-=1;
+    for (int i = 1 ; i < argc ; ++i){
+        if (!strcmp(argv[i],"-p")){
+            PATH = argv[i+1];
+            ++i;
+        }
+    }
+
+    std::cout << "Initalizing CrowSqlite 0.1v-alpha";
+    sqlite3* db;
+
+    if(sqlite3_open_v2(PATH.c_str(),&db,SQLITE_OPEN_READWRITE,nullptr) != SQLITE_OK){
         throw std::runtime_error("Failed to open Database File");
     }
 
@@ -130,17 +173,15 @@ int main(){
     //crowsqlite.loglevel(crow::LogLevel::Debug);
 
 
-    CROW_ROUTE(crowsqlite, "/api/<string>")([](std::string sql){        
+    CROW_ROUTE(crowsqlite, "/api/<string>")([&](std::string sql){        
         std::string result = "";
         int crow_err = 0;
-        request_sqlite(sql, result, crow_err);
+        request_sqlite(db, sql, result, crow_err);        
         if(!crow_err){
-            std::cout << result.c_str() << "Done here";
-
             return result;
         }else{
-            std::cout << result.c_str() << "Done here";
-        return result;
+            result = "";
+            return result; // Should we send the message or not?
         }
     });
 
