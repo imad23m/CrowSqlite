cf33226251efba122a13ade3b837f4043116b7c2
Imad Laggoune <imad.laggoune.m@gmail.com>
2024-06-06 11:33:02 +0100

    [ADD] sqlite3_prepare support
    -Now you could prepare statements and use them as often as you want using /pre/SQL and /use/INDEX
    -changed /api to /sql

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,8 @@ MIT License | Copyright (c) 2024 Imad Laggoune
 #include <iostream>
 #include <string>
 
-
-//char* sqlite_err = nullptr;
-//std::string result;
+// char* sqlite_err = nullptr;
+// std::string result;
 
 /*
 int request_sqlite_callback(void* args, int size, char** content, char** name){
@@ -34,73 +33,73 @@ result->append("{");
     return 0;
 }*/
 
+int request_sqlite_callback(void* args, int size, char** content, char** name)
+{
 
-int request_sqlite_callback(void* args, int size, char** content, char** name){
-
-std::string* result = (std::string*)(((void **)args)[0]);
-    int* index = (int*)(((void **)args)[1]);
+    std::string* result = (std::string*)(((void**)args)[0]);
+    int* index = (int*)(((void**)args)[1]);
 
     result->append("\n\t\"");
     result->append(std::to_string(*index));
-    //*index +=1;
     ++*index;
     result->append("\" : {");
 
-    for (int i = 0; i < size; ++i){
+    for (int i = 0; i < size; ++i) {
         result->append("\n\t\t\"");
         result->append(name[i]);
-        result->append( "\" : \"");
-        result->append( content[i]);
+        result->append("\" : \"");
+        result->append(content[i]);
         result->append("\",");
     }
     result->pop_back();
-    result ->append( "\n\t\t},"); 
+    result->append("\n\t\t},");
 
     return 0;
 }
 
-
-const char* url_decoder(std::string& url_encoded_string){
+const char* url_decoder(std::string& url_encoded_string)
+{
 
     int size = url_encoded_string.length();
 
-    for (int i = 0; i < size; ++i){
-        if (url_encoded_string[i] == '%'){
-            if (url_encoded_string[i+1] == '2'){
+    for (int i = 0; i < size; ++i) {
+        if (url_encoded_string[i] == '%') {
+            if (url_encoded_string[i + 1] == '2') {
 
-                if (url_encoded_string[i+2] == '0'){
+                if (url_encoded_string[i + 2] == '0') {
                     url_encoded_string[i] = ' ';
                 }
 
-                if (url_encoded_string[i+2] == '2'){
+                if (url_encoded_string[i + 2] == '2') {
                     url_encoded_string[i] = '"';
                 }
-                url_encoded_string.erase(url_encoded_string.begin()+i+1,url_encoded_string.begin()+i+3);
-                size-= 2;
+                url_encoded_string.erase(url_encoded_string.begin() + i + 1, url_encoded_string.begin() + i + 3);
+                size -= 2;
             }
         }
     }
     return url_encoded_string.c_str();
 }
 
-int request_sqlite(sqlite3* &db, std::string sql, std::string &result, int &crow_err){
+int request_sqlite(sqlite3*& db, std::string sql, std::string& result, int& crow_err)
+{
 
-    const char* sql_c = url_decoder(sql);    
+    const char* sql_c = url_decoder(sql);
     char* sqlite_err = nullptr;
     int err = 0;
     int index = 0;
 
-    void* arguments[2] = {&result, &index};
+    void* arguments[2] = { &result, &index };
 
     result = "{";
-    err = sqlite3_exec(db,sql_c,request_sqlite_callback,&arguments, &sqlite_err);
+    err = sqlite3_exec(db, sql_c, request_sqlite_callback, &arguments, &sqlite_err);
     if (result != "{")
-         result.pop_back();
+        result.pop_back();
     result += "\n}";
 
-    if (err == SQLITE_OK){
+    if (err == SQLITE_OK) {
         return 0;
-    } else{
+    } else {
         std::cout << "[SQLITE3][ERROR] " << sqlite_err << "\n";
         crow_err = 1;
         result = sqlite_err;
@@ -108,56 +107,109 @@ int request_sqlite(sqlite3* &db, std::string sql, std::string &result, int &crow
     }
 }
 
-
-
-
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
 
     std::string PATH; // NEED absolute path, TODO: Fix this
     int PORT = 12000;
-   // argc-=1;
-    for (int i = 1 ; i < argc ; ++i){
+    // argc-=1;
+    for (int i = 1; i < argc; ++i) {
         std::cout << argv[i] << "\n";
-        if (!strcmp(argv[i],"-d")){
-            PATH = argv[i+1];
+        if (!strcmp(argv[i], "-d")) {
+            PATH = argv[i + 1];
             ++i;
         }
 
-        if (!strcmp(argv[i],"-p")){
-            PORT = std::__cxx11::stoi(argv[i+1]); // Maybe we should find another solution
+        if (!strcmp(argv[i], "-p")) {
+            PORT = std::__cxx11::stoi(argv[i + 1]); // Maybe we should find another solution
             ++i;
         }
 
-        if (!strcmp(argv[i],"-h")){
-            std::cout << "CrowSqlite 0.1v\nMinimal Sqlite Server\n\nArgument List:\n\n\t -d\t\t Absolute path to the database file\n\t -p\t\t Port Number (Default: 12000)\n\t -h\t\t Print help text\n\n";
+        if (!strcmp(argv[i], "-h")) {
+            std::cout << "CrowSqlite 0.1v\nMinimal Sqlite Server\n\nArgument List:\n\n\t "
+                         "-d\t\t Absolute path to the database file\n\t -p\t\t Port Number "
+                         "(Default: 12000)\n\t -h\t\t Print help text\n\n";
             return 0;
         }
     }
 
     std::cout << "Initalizing CrowSqlite 0.1v";
-    if(PATH.empty()){
-        std::cout << "ERROR: No path for database file specified, please use argument -d PATH.\n";
+    if (PATH.empty()) {
+        std::cout << "ERROR: No path for database file specified, please use "
+                     "argument -d PATH.\n";
+        return 1;
     }
     sqlite3* db;
 
-    if(sqlite3_open_v2(PATH.c_str(),&db,SQLITE_OPEN_READWRITE,nullptr) != SQLITE_OK){
+    if (sqlite3_open_v2(PATH.c_str(), &db, SQLITE_OPEN_READWRITE, nullptr) != SQLITE_OK) {
         throw std::runtime_error("Failed to open Database File");
     }
 
     crow::SimpleApp crowsqlite;
-    crowsqlite.loglevel(crow::LogLevel::Critical);
+    // crowsqlite.loglevel(crow::LogLevel::Critical);
     crowsqlite.port(PORT);
+    std::vector<sqlite3_stmt*> statements;
 
-    CROW_ROUTE(crowsqlite, "/api/<string>")([&](std::string sql){        
+    CROW_ROUTE(crowsqlite, "/pre/<string>")
+    ([&](std::string sql) {
+        sqlite3_stmt* statement;
+        const char* sql_c = url_decoder(sql);
+        std::cout << "Going to prepare\n";
+        if (sqlite3_prepare(db, sql_c, -1, &statement, nullptr) != SQLITE_OK) {
+            return "Error";
+        } else {
+            std::cout << "Going to done\n";
+            if (statements.empty()) {
+                std::cout << "Impressive, its empty\n";
+            }
+            statements.push_back(statement);
+            return "Done";
+        }
+    });
+
+    CROW_ROUTE(crowsqlite, "/use/<int>")
+    ([&](int index) {
+        int err;
+        std::string result = "{";
+        int index_c = 0;
+        do {
+            err = sqlite3_step(statements[index]);
+            if (err == SQLITE_ROW) {
+                int size = sqlite3_column_count(statements[index]);
+
+                result += "\n\t\"";
+                result += std::to_string(index_c);
+                result += "\" : {";
+                for (int i = 0; i < size; ++i) {
+                    result += "\n\t\t\"";
+                    result += sqlite3_column_name(statements[index], i);
+                    result += "\" : \"";
+                    result += (const char*)sqlite3_column_text(statements[index], i);
+                    result += "\",";
+                }
+                result.pop_back();
+                result += "\n\t\t},";
+            }
+
+            ++index_c;
+        } while (err != SQLITE_DONE);
+        if (result != "{")
+            result.pop_back();
+        result += "\n}";
+        std::cout << result << "\n";
+        return result;
+    });
+
+    CROW_ROUTE(crowsqlite, "/sql/<string>")
+    ([&](std::string sql) {
         std::string result = "";
         int crow_err = 0;
-        request_sqlite(db, sql, result, crow_err);        
-        if(!crow_err){
+        request_sqlite(db, sql, result, crow_err);
+        if (!crow_err) {
             return result;
-        }else{
+        } else {
             result = "";
-            return result; // Should we send the message or not? PROS: one could debug using browers | CONS: it could lead to an unexpected error depending on 
-                            // the software it will handle its output, probably a json detection in the target software will do the job.
+            return result;
         }
     });
     crowsqlite.multithreaded().run();
